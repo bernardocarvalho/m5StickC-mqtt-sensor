fae164ed8cf664623e70275a79afcb72be4a8ebb
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-12-05 10:37:05 +0000

    Period 30
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 /*
-* vim: sta:et:sw=4:ts=4:sts=4
-*  * @file
-*******************************************************************************
-* Copyright (c) 2021 by M5Stack
-*                  Equipped with M5StickC sample source code
-*                          配套  M5StickC 示例源代码
-* Visit for more information: https://docs.m5stack.com/en/core/m5stickc
-*
-* Describe:  NTP TIME.
-* Date: 2021/8/3
-* https://docs.m5stack.com/en/core/m5stickc
-* https://docs.m5stack.com/en/unit/watering
-* https://github.com/m5stack/M5Stack/blob/master/examples/Unit/WATERING/WATERING.ino
-* https://console.hivemq.cloud/clients/arduino-esp8266?uuid=e6c3a2b784ad4434b0238f17f98eac34
-* https://github.com/m5stack/M5StickC/blob/master/examples/Advanced/MQTT/MQTT.ino
-******************************************************************************
-*/
+ * vim: sta:et:sw=4:ts=4:sts=4
+ *  * @file
+ *******************************************************************************
+ * Copyright (c) 2021 by M5Stack
+ *                  Equipped with M5StickC sample source code
+ *                          配套  M5StickC 示例源代码
+ * Visit for more information: https://docs.m5stack.com/en/core/m5stickc
+ *
+ * Describe:  NTP TIME.
+ * Date: 2021/8/3
+ * https://docs.m5stack.com/en/core/m5stickc
+ * https://docs.m5stack.com/en/unit/watering
+ * https://github.com/m5stack/M5Stack/blob/master/examples/Unit/WATERING/WATERING.ino
+ * https://console.hivemq.cloud/clients/arduino-esp8266?uuid=e6c3a2b784ad4434b0238f17f98eac34
+ * https://github.com/m5stack/M5StickC/blob/master/examples/Advanced/MQTT/MQTT.ino
+ ******************************************************************************
+ */
 
 #include <Arduino.h>
 
@@ -23,18 +23,15 @@
 #include <WiFi.h>
 
 //#include <NTPClient.h>
-//#include <time.h>
 //#include <TZ.h>
 //#include <FS.h>
-//#include <PubSubClient.h>
 
 #include "time.h"
-
-// Set the name and password of the wifi to be connected.
 
 #include <ArduinoMqttClient.h>
 #include <ArduinoJson.h>
 
+// Set the name and password of the wifi to be connected.
 #ifndef STASSID
 #include "arduino_secrets.h"
 #endif
@@ -44,20 +41,17 @@ const int WATER_AUTO = 2UL; // in sec. Auto water/ day
 const char *ssid = STASSID;
 const char *password = STAPSK;
 
-const char* mqtt_server  = "test.mosquitto.org";
 const char* mqtt_broker  = "test.mosquitto.org";
 const int mqtt_port = 1883;
+const int msgPeriod = 30 * 1000U;
 
 #define INPUT_PIN 33 //  SCL
 #define PUMP_PIN  32 //  SDA
 
-//const char* ssid     = "A52_BBC";
 bool led_state = false;
 
 WiFiClient espClient;
 MqttClient mqttClient(espClient);
-WiFiUDP ntpUDP;
-//NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
 const char willTopic[] = "ipfn/m5stick/will";
 const char inTopic[]   = "ipfn/m5stick/in";
@@ -65,7 +59,8 @@ const char outTopic[]  = "ipfn/m5stick/out";
 
 const char* ntpServer = "ntp1.tecnico.ulisboa.pt";  // Set the connect NTP server.
 const long gmtOffset_sec     = 0;
-const int daylightOffset_sec = 3600;
+const int daylightOffset_sec = 0; // 3600;
+
 #define MSG_BUFFER_SIZE 50
 char msg[MSG_BUFFER_SIZE];
 
@@ -74,23 +69,10 @@ unsigned int sumWater = 0;
 
 unsigned long stopPump = 0, nextWater;
 
-#define SERIAL_PRINTF_MAX_BUFF      256
-void serialPrintf(const char *fmt, ...);
-void serialPrintf(const char *fmt, ...) {
-    // Buffer for storing the formatted data
-    char buff[SERIAL_PRINTF_MAX_BUFF];
-    // pointer to the variable arguments list
-    va_list pargs;
-    // Initialise pargs to point to the first optional argument
-    va_start(pargs, fmt);
-    // create the formatted data and store in buff
-    vsnprintf(buff, SERIAL_PRINTF_MAX_BUFF, fmt, pargs);
-    va_end(pargs);
-    Serial.print(buff);
-}
-
+bool ntpOK = false;
 void printLocalTime() {  // Output current time.
     struct tm timeinfo;
+    M5.Lcd.setCursor(0, 20);
     if (!getLocalTime(&timeinfo)) {  // Return 1 when the time is successfully
         // obtained.
         M5.Lcd.println("Failed to obtain time");
@@ -100,7 +82,9 @@ void printLocalTime() {  // Output current time.
             "%A, %B %d \n%Y %H:%M:%S");  // Screen prints date and time.
     Serial.println(&timeinfo,
             "%A, %B %d %Y %H:%M:%S");  //prints date and time.
-    strftime(msg, MSG_BUFFER_SIZE, "%A, %B %d %Y %H:%M:%S", &timeinfo);
+    //strftime(msg, MSG_BUFFER_SIZE, "%A %B %d %Y %H:%M:%S", &timeinfo);
+    // ISO 8601 UTC
+    strftime(msg, MSG_BUFFER_SIZE, "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
 
 }
 void setup_wifi() {
@@ -119,7 +103,6 @@ void setup_wifi() {
     }
 
     randomSeed(micros());
-    //timeClient.begin();
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
@@ -172,12 +155,6 @@ void setup() {
     M5.Lcd.printf("\nConnecting to %s", ssid);
     Serial.begin(115200);
     setup_wifi();
-    /*
-       mqttClientP.setServer(mqtt_server,
-       1883);  // Sets the server details.
-       mqttClientP.setCallback(
-       callback);  // Sets the message callback function.
-       */
     String willPayload = "oh no!";
     bool willRetain = true;
     int willQos = 1;
@@ -204,6 +181,7 @@ void setup() {
     // subscribe to a topic
     // the second parameter sets the QoS of the subscription,
     // the library supports subscribing at QoS 0, 1, or 2
+    // QoS 1  guarantees that the message will be transferred successfully to the broker.
     int subscribeQos = 1;
 
     mqttClient.subscribe(inTopic, subscribeQos);
@@ -212,7 +190,36 @@ void setup() {
     // mqttClient.unsubscribe(inTopic);
 
     configTime(gmtOffset_sec, daylightOffset_sec,
-            ntpServer, "pool.ntp.org", "time.nist.gov");  // init and get the time.
+            ntpServer, "pool.ntp.org", "time.nist.gov");  // init and get the time .
+    ntpOK = true;
+    struct tm ntpTime;
+    RTC_TimeTypeDef RTC_TimeStruct;
+    M5.Rtc.GetTime(&RTC_TimeStruct);  // Gets the time in the real-time clock.
+    snprintf(msg, MSG_BUFFER_SIZE, "RTC Time H:%02d M:%02d S: %02d\n", RTC_TimeStruct.Hours,
+            RTC_TimeStruct.Minutes, RTC_TimeStruct.Seconds);
+    Serial.println(msg);
+
+    RTC_DateTypeDef RTC_DateStruct;
+    if (getLocalTime(&ntpTime)) {  // Return 1 when the time is successfully
+        // obtained.
+        ntpOK = true;
+
+        RTC_TimeStruct.Hours   = ntpTime.tm_hour;  // Set the time.  设置时间
+        RTC_TimeStruct.Minutes = ntpTime.tm_min;
+        RTC_TimeStruct.Seconds = ntpTime.tm_min;
+        M5.Rtc.SetTime(&RTC_TimeStruct);  // and writes the set time to the real time
+        // clock.
+        //RTC_DateStruct.WeekDay = 3;  // Set the date.
+        RTC_DateStruct.Month   = ntpTime.tm_mon;
+        RTC_DateStruct.Date    = ntpTime.tm_mday;
+        RTC_DateStruct.Year    = ntpTime.tm_year;
+        M5.Rtc.SetData(&RTC_DateStruct);
+    }
+    else{
+        Serial.println("Failed to obtain NTP time");
+        ntpOK = false;
+    }
+
     // printLocalTime();
     //WiFi.mode(WIFI_OFF);  // Set the wifi mode to off.
     unsigned long now = millis();
@@ -239,28 +246,20 @@ void loop() {
     }
     //
     mqttClient.poll();
-    if (now - lastMsg > 2000) {
+    now = millis();
+    if (now - lastMsg > msgPeriod) {
         lastMsg = now;
 
         rawADC = analogRead(INPUT_PIN);
-        snprintf(msg, MSG_BUFFER_SIZE, "Watering ADC value: %ld",
-                rawADC);
-        Serial.println(msg);
-        //Serial.print("Watering ADC value: ");
-        //Serial.println(rawADC);
-        M5.Lcd.setCursor(40, 100);
+        //Serial.print(now/1000);
+        snprintf(msg, MSG_BUFFER_SIZE, "%u, Humid ADC: %ld, ", now/1000, rawADC);
+        Serial.print(msg);
+        //M5.Lcd.setCursor(0, 25);  // Set cursor to (0,25).
+        M5.Lcd.setCursor(0, 40);
         M5.Lcd.print(msg);
-        //M5.Lcd.print("ADC: " + String(rawADC));
-        // topic.
+       // M5.Lcd.print("\nADC: " + String(rawADC));
 
-        M5.Lcd.setCursor(0, 25);  // Set cursor to (0,25).
-        //timeClient.update();
-
-        //    Serial.println(timeClient.getFormattedTime());
-
-        doc["msg"]   = msg;
-
-        printLocalTime(); // Time string will be on msq
+        printLocalTime(); // Time string will be on msg
         doc["time"]  = msg;
         doc["Humid"] = rawADC;
         doc["sumWater"] = sumWater;
@@ -269,8 +268,38 @@ void loop() {
         doc["Bat_V"]   =  M5.Axp.GetBatVoltage();
         doc["In5_V"]   =  M5.Axp.GetVinVoltage();
         doc["In5_I"]   =  M5.Axp.GetVinCurrent();
+        doc["USB_V"]   =  M5.Axp.GetVBusVoltage();
         doc["USB_I"]   =  M5.Axp.GetVBusCurrent();
-        /*
+        bool retained = false;
+
+        int publishQos = 1;
+        bool dup = false;
+
+        mqttClient.beginMessage(outTopic,  (unsigned long)measureJson(doc), retained, publishQos, dup);
+        serializeJson(doc, mqttClient);
+        mqttClient.endMessage();
+    }
+}
+
+/*
+#define SERIAL_PRINTF_MAX_BUFF      256
+void serialPrintf(const char *fmt, ...);
+void serialPrintf(const char *fmt, ...) {
+    // Buffer for storing the formatted data
+    char buff[SERIAL_PRINTF_MAX_BUFF];
+    // pointer to the variable arguments list
+    va_list pargs;
+    // Initialise pargs to point to the first optional argument
+    va_start(pargs, fmt);
+    // create the formatted data and store in buff
+    vsnprintf(buff, SERIAL_PRINTF_MAX_BUFF, fmt, pargs);
+    va_end(pargs);
+    Serial.print(buff);
+}
+*/
+        
+/*
+         *
            display.printf("Bat:\r\n  V: %.3fv  I: %.3fma\r\n", M5.Axp.GetBatVoltage(),
            M5.Axp.GetBatCurrent());
            display.printf("USB:\r\n  V: %.3fv  I: %.3fma\r\n", M5.Axp.GetVBusVoltage(),
@@ -278,13 +307,4 @@ void loop() {
            display.printf("5V-In:\r\n  V: %.3fv  I: %.3fma\r\n",
            M5.Axp.GetVinVoltage(), M5.Axp.GetVinCurrent());
            display.printf("Bat power %.3fmw", M5.Axp.GetBatPower());
-           */
-        bool retained = false;
-        int qos = 1;
-        bool dup = false;
-
-        mqttClient.beginMessage(outTopic,  (unsigned long)measureJson(doc), retained, qos, dup);
-        serializeJson(doc, mqttClient);
-        mqttClient.endMessage();
-    }
-}
+*/
